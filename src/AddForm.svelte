4e82fef4c84f3e125f08e3a744a1cf73677c391e
Daniel Wang <wangxxx.dy@gmail.com>
2022-02-07 22:49:35 +0900

    try on svelte basics

---
+++ src/AddForm.svelte
@@ -0,0 +1,38 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  let dispatch = createEventDispatcher();
+
+  let name;
+  let color;
+  let age;
+
+  let skills = [];
+
+  const handleSubmit = () => {
+    const newOne = {
+      name: name,
+      color: color,
+      age: age,
+      skills: skills,
+      id: Math.random(),
+    };
+    dispatch("addOne", newOne);
+  };
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <input type="text" placeholder="name" bind:value={name} />
+  <label for="color" />
+  <select name="color" bind:value={color}>
+    <option value="red">Red</option>
+    <option value="white">White</option>
+    <option value="black">Black</option>
+  </select>
+  <input type="number" placeholder="age" bind:value={age} />
+  <label for="">Skills</label>
+  <input type="checkbox" bind:group={skills} value="Vue" /> Vue <br />
+  <input type="checkbox" bind:group={skills} value="React" /> React <br />
+  <input type="checkbox" bind:group={skills} value="Svelte" /> Svelte <br />
+  <button>Add</button>
+</form>
--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,43 @@
 <script>
   import Modal from "./Modal.svelte";
+  import AddForm from "./AddForm.svelte";
+
+  let showModal = false;
 
   let people = [
-    { name: "Xu", color: "red", age: 18, id: 1 },
-    { name: "Don", color: "pink", age: 35, id: 2 },
-    { name: "Pi", color: "black", age: 24, id: 3 },
+    { name: "Xu", color: "red", age: 18, skills: [], id: 1 },
+    { name: "Don", color: "pink", age: 35, skills: [], id: 2 },
+    { name: "Pi", color: "black", age: 24, skills: [], id: 3 },
   ];
 
   const handleClick = (id) => {
     people = people.filter((one) => one.id != id);
   };
+
+  const togleModal = () => {
+    showModal = !showModal;
+  };
+
+  const addOne = (e) => {
+    const newOne = e.detail;
+    people = [newOne, ...people];
+    showModal = false;
+  };
 </script>
 
-<Modal />
-
+<Modal {showModal} on:click={togleModal}>
+  <AddForm on:addOne={addOne} />
+</Modal>
 <main>
-  {#each people as one}
+  <button on:click|once={togleModal}>Toggle</button>
+  {#each people as one (one.id)}
     <div>
       <h1>{one.name}</h1>
       {#if one.color === "black"}
         <p>Master</p>
       {/if}
       <p>{one.age} years old, seems {one.color}</p>
+      <p>{one.skills}</p>
       <button on:click={() => handleClick(one.id)}>delete</button>
     </div>
   {:else}
